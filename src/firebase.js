5d2da7f517f96d0ea61c0666aaccb185b63aa30b
Yuval Hazut <hazuty@gmail.com>
2025-09-11 21:56:15 +0300

    Fix: export db from firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,21 +1,20 @@
-// Import the functions you need from the SDKs you need
+// src/firebase.js
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getFirestore, enableIndexedDbPersistence } from "firebase/firestore";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// ⚠️ בדיוק כפי שמופיע אצלך בקונסול (מהצילום שהבאת)
 const firebaseConfig = {
-  apiKey: "AIzaSyA_gdKvRRlllPu5ivzj_DgvdJ_1gbgzG1A",
+  apiKey: "AIzaSyA_gdKvRR1llPu5ivzj_DgvdJ_1gbgzG1A",
   authDomain: "teameter-562a1.firebaseapp.com",
   projectId: "teameter-562a1",
   storageBucket: "teameter-562a1.firebasestorage.app",
   messagingSenderId: "1061485896783",
   appId: "1:1061485896783:web:869c2875682fa24cb0763a",
-  measurementId: "G-0WWWS7X7SJ"
+  // measurementId לא נדרש לסנכרון
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+export const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
+
+// אופציונלי: עבודה גם בלי אינטרנט; לא קריטי
+enableIndexedDbPersistence(db).catch(() => {});
